da1b2c7d95f456540bd788fc89d7bbda9029ef70
ViniciusNovaes <vinicius.novaes@vtex.com.br>
2022-08-02 09:13:22 -0300

    - Removing unused clients

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,10 +1,4 @@
 import { IOClients } from '@vtex/api'
 
-import SupplierBuilder from './suppliersBuilder'
-
 // Extend the default IOClients implementation with our own custom clients.
-export class Clients extends IOClients {
-  public get supplierBuilder() {
-    return this.getOrSet('supplierBuilder', SupplierBuilder)
-  }
-}
+export class Clients extends IOClients {}
