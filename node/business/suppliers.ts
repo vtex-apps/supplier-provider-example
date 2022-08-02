380cf2c72c753809c5361d6401c96f8acc0c1534
ViniciusNovaes <vinicius.novaes@vtex.com.br>
2022-08-02 09:08:52 -0300

    - Adding comments and using interface

--- node/business/suppliers.ts
+++ node/business/suppliers.ts
@@ -1,6 +1,17 @@
 import type { ProtocolRequest } from '../typings/protocolRequest'
 import { generateSupplier } from './mock'
+import type { Supplier } from '../typings/supplier'
 
 export async function getSuppliers(payload: ProtocolRequest) {
-  return [await generateSupplier(payload), await generateSupplier(payload)]
+  /*
+   *
+   * This is the function you must implement as you will fetch yours suppliers
+   *
+   */
+  const providedSuppliers: Supplier[] = [
+    await generateSupplier(payload),
+    await generateSupplier(payload),
+  ]
+
+  return providedSuppliers
 }
